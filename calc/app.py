d0a24cf98417c11b6b58b3a79bdea00ce44e862f
Rijo George <rijogeorge@macbook-pro-2.lan>
2021-12-05 16:32:51 -0800

    not tested code

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,19 @@
 # Put your app in here.
+from operations import *
+
+@app.route('add')
+def add_route():
+    return add(request.args["a"], request.args["b"])
+
+@app.route('sub')
+def sub_route():
+    return sub(request.args["a"], request.args["b"])
+
+@app.route('mult')
+def mult_route():
+    return mult(request.args["a"], request.args["b"])
+
+@app.route('div')
+def div_route():
+    return div(request.args["a"], request.args["b"])
+
